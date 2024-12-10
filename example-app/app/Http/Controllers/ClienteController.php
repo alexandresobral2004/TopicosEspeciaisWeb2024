d8f7b6a23f7603211c2be6e710ae6ada7d37893b
Alexandre Rocha <alexandresobral2004@gmail.com>
2024-12-10 17:46:16 -0300

    commit1

---
+++ example-app/app/Http/Controllers/ClienteController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cliente;
+use Illuminate\Http\Request;
+
+class ClienteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $clientes = Cliente::all();
+        return view('clientes.index', compact('clientes'));     
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('clientes.create',['cliente' => new Cliente()]);
+       
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nome' => 'required',
+            'email' => 'required',
+            'telefone' => 'required',
+        ]);
+
+        $cliente = new Cliente();
+        $cliente->nome = $request->input('nome');
+        $cliente->email = $request->input('email');
+        $cliente->telefone = $request->input('telefone');
+        $cliente->save();
+        return redirect()->route('clientes.index');
+        
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Cliente $cliente)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Cliente $cliente)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Cliente $cliente)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Cliente $cliente)
+    {
+        //
+    }
+}
---
+++ example-app/resources/views/clientes/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.main')
+
+@section('content')
+<h1>Bem vindo a Página de clientes</h1>
+
+@endsection
